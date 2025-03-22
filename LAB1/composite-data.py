5e7e3a67dcc1306ef4c8ac975f848876acb91c34
Ifeoma <osegboifeoma3@gmail.com>
2025-03-22 16:32:47 +0100

    Update Composite-data.py
    Created a new category and Using the For loop and the If and else Statements.

--- LAB1/composite-data.py
+++ LAB1/composite-data.py
@@ -16,3 +16,27 @@ myVehicle = {
 #checking the key values for the my vehicle dictionary
 for key, value in myVehicle.items():
     print("{} : {}".format(key,value))
+#creating a new category (the category below is empty)
+myInventoryList = []
+#opening the csv file that was created with the command below (since my csv file is not a root file but in a folder i used the ('copy relative path/the csv file name))
+with open('LAB1/car_fleet.csv') as csvFile:
+    csvReader = csv.reader(csvFile, delimiter=',')  #reading the csv file
+    lineCount = 0  
+    for row in csvReader:
+            if lineCount == 0: #((== two is for comparison while =one is for assigning))
+                print(f'Column names are: {", ".join(row)}')  
+                lineCount += 1  
+            else:  
+                print(f'vin: {row[0]} make: {row[1]}, model: {row[2]}, year: {row[3]}, range: {row[4]}, topSpeed: {row[5]}, zeroSixty: {row[6]}, mileage: {row[7]}')  
+                currentVehicle = copy.deepcopy(myVehicle)  
+                currentVehicle["vin"] = row[0]  
+                currentVehicle["make"] = row[1]  
+                currentVehicle["model"] = row[2]  
+                currentVehicle["year"] = row[3]  
+                currentVehicle["range"] = row[4]  
+                currentVehicle["topSpeed"] = row[5]  
+                currentVehicle["zeroSixty"] = row[6]  
+                currentVehicle["mileage"] = row[7]  
+                myInventoryList.append(currentVehicle)  
+                lineCount += 1  
+    print(f'Processed {lineCount} lines.')

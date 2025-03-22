3340bea968c6a9444bfd695cada297ba40a4dd73
Ifeoma <osegboifeoma3@gmail.com>
2025-03-22 16:25:34 +0100

    Adding a dictionary
    Adding a dictionary of my Vehicles and checking the values.

--- LAB1/composite-data.py
+++ LAB1/composite-data.py
@@ -1,3 +1,18 @@
 #importing the csv file and copying
 import csv
 import copy
+
+#the dictionary of my vehicle
+myVehicle = {
+        "vin" : "<empty>",
+        "make" : "<empty>" ,
+        "model" : "<empty>" ,
+        "year" : 0,
+        "range" : 0,
+        "topSpeed" : 0,
+        "zeroSixty" : 0.0,
+        "mileage" : 0
+    }
+#checking the key values for the my vehicle dictionary
+for key, value in myVehicle.items():
+    print("{} : {}".format(key,value))
